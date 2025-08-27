d5fa87504cf4af35ec45f5f8bcfc9c2e2fa20979
Tam Thong <TamThong@mail.com>
2025-08-27 15:28:02 +0700

    initial

---
+++ src/main/java/com/chill_guys/vitae_backend/VitaeBackendApplication.java
@@ -0,0 +1,13 @@
+package com.chill_guys.vitae_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VitaeBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VitaeBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chill_guys/vitae_backend/VitaeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chill_guys.vitae_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VitaeBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
